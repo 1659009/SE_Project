4761a290e8d39267696e6765efe1e00c5178207c
Hai To <tokyhai0811@gmail.com>
2019-07-10 18:49:27 +0700

    Fake data.

--- src/views/Homepage/Homepageview.jsx
+++ src/views/Homepage/Homepageview.jsx
@@ -9,22 +9,42 @@ import { Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(theme => ({}));
 
-}));
-
- export default function Homepageview(){
-    const classes = useStyles();
- 
-    return (
-      <div>
-        <Homepage />
-        <Homepagemainpic/>
-        <ImgMediaCard/>
-        <Footer/>
-      </div>
-    );
-
+export default function Homepageview() {
+  const classes = useStyles();
+  const data = [
+    {
+      title: '123',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    {
+      title: '456',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    {
+      title: '789',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    {
+      title: '123',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    {
+      title: '456',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    {
+      title: '789',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    }
+  ];
+  return (
+    <div>
+      <Homepage />
+      <Homepagemainpic />
+      <ImgMediaCard data={data} />
+      <Footer />
+    </div>
+  );
 }
-
-
--- src/views/Homepage/components/Homepagecontent.jsx
+++ src/views/Homepage/components/Homepagecontent.jsx
@@ -19,12 +19,50 @@ const useStyles = makeStyles({
   }
 });
 
-export default function ImgMediaCard() {
+export default function ImgMediaCard(props) {
   const classes = useStyles();
-
+  const { data } = props;
+  console.log(props);
   return (
-    <div>
-      <Container style={{ marginTop: '10vh' }}>
+    <div className="container">
+      <div className="row" style={{ marginTop: '25px', padding: 'auto' }}>
+        {data.map(data => {
+          return (
+            <div className="col-4" style={{ paddingTop: '20px' }}>
+              <Card className={classes.card}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt="Contemplative Reptile"
+                    height="140"
+                    image="https://ak0.picdn.net/shutterstock/videos/8911510/thumb/1.jpg"
+                    title="Contemplative Reptile"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {data.title}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p">
+                      {data.content}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Enroll
+                  </Button>
+                  <Button size="small" color="primary">
+                    View
+                  </Button>
+                </CardActions>
+              </Card>
+            </div>
+          );
+        })}
+        {/* <Container style={{ marginTop: '10vh' }}>
         <Grid container spacing={1} className={classes.cardContent}>
           <Grid item xs>
             <Card className={classes.card}>
@@ -241,7 +279,8 @@ export default function ImgMediaCard() {
             </Card>
           </Grid>
         </Grid>
-      </Container>
+      </Container> */}
+      </div>
     </div>
   );
 }
