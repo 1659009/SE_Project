2fa8e8e9dccf21e2561b38315f77d9f8d3405f76
Hai To <tokyhai0811@gmail.com>
2019-08-09 10:39:09 +0700

    FB link

--- src/layouts/Homepage/components/Footer/Footer.jsx
+++ src/layouts/Homepage/components/Footer/Footer.jsx
@@ -9,21 +9,34 @@ const useStyles = makeStyles(theme => ({}));
 export default function Footer() {
   const classes = useStyles();
   return (
-    <Paper style={{ paddingTop: '10px', height: '30vh', background: '#d0b808' }}>
+    <Paper
+      style={{ paddingTop: '10px', height: '30vh', background: '#d0b808' }}>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6} style={{ position: 'relative' }}>
           <div style={{ paddingLeft: '10%', paddingTop: '5vh' }}>
             <h1>CONTACT</h1>
             <MailOutline /> ABC@abc.com <br />
-            <Phone /> 01256842315<br />
-            <Home /> 369 Ba Hat Street, Ward 4, District 10<br />
+            <Phone /> 01256842315
+            <br />
+            <Home /> 369 Ba Hat Street, Ward 4, District 10
+            <br />
           </div>
         </Grid>
         <Grid item xs={12} sm={6}>
           <div style={{ textAlign: 'center', paddingTop: '5vh' }}>
             <h1>CONNECT TO US</h1>
-            <img src="https://cdn3.iconfinder.com/data/icons/sympletts-part-2/128/circle-social-facebook-512.png" style={{ width: '8%', height: '8%', marginRight: '100px' }} alt="Facebook" />
-            <img src="https://cdn2.iconfinder.com/data/icons/instagram-new/512/instagram-round-liner-512.png" style={{ width: '7%', height: '7%' }} alt="Instagram" />
+            <a href="https://www.facebook.com/soen.education/" target="_blank">
+              <img
+                src="https://cdn3.iconfinder.com/data/icons/sympletts-part-2/128/circle-social-facebook-512.png"
+                style={{ width: '8%', height: '8%', marginRight: '100px' }}
+                alt="Facebook"
+              />
+            </a>
+            <img
+              src="https://cdn2.iconfinder.com/data/icons/instagram-new/512/instagram-round-liner-512.png"
+              style={{ width: '7%', height: '7%' }}
+              alt="Instagram"
+            />
           </div>
         </Grid>
       </Grid>
